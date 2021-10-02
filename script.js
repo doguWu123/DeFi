171d83cae19842adfaad3bd96236cea82def6310
Neev Shah <75965411+nfhneevns@users.noreply.github.com>
2021-10-02 13:15:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyCnOJcMO99fi-Zg12BDI-QYXFhHNeM_t88",
-    authDomain: "billbuddy-a1e84.firebaseapp.com",
-    projectId: "billbuddy-a1e84",
-    storageBucket: "billbuddy-a1e84.appspot.com",
-    messagingSenderId: "914583831053",
-    appId: "1:914583831053:web:f8ff36db1190a704e54b44"
+    apiKey: "123",
+    authDomain: "123",
+    projectId: "123",
+    storageBucket: "123",
+    messagingSenderId: "123",
+    appId: "123"
   };
   
   // Initialize Firebase
